ad421a74bff7db82cf0c9a844769214ee35fa565
John Imeobong <imeobongjohn38@gmail.com>
2025-08-13 22:02:39 +0100

    Delete Diary/src/main/java/com.diary/data/models directory

--- Diary/src/main/java/com.diary/data/models/Pages.java
+++
@@ -1,4 +0,0 @@
-package main.java.com.diary.data.models;
-
-public class Pages {
-}
--- Diary/src/main/java/com.diary/data/models/User.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.diary.data.models;
-
-
-public class User {
-    private String id;
-    private String name;
-    private String password;
-}
